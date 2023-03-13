495a26c9993d3ff72dfa00170b36348dbd10f6d5
Angel Motta <angel.motta@utec.edu.pe>
2023-03-12 19:10:35 -0500

    refactor: add languages

--- helloWorld/hello_test.go
+++ helloWorld/hello_test.go
@@ -4,14 +4,26 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Angel")
+		got := Hello("Angel", "")
 		want := "Hello, Angel"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Angel", "Spanish")
+		want := "Hola, Angel"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Angel", "French")
+		want := "Bonjour, Angel"
 		assertCorrectMessage(t, got, want)
 	})
 }
