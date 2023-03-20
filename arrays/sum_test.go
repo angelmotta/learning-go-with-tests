c0a52864f03f28902b5cedd11870fe8459cff155
Angel Motta <angel.motta@utec.edu.pe>
2023-03-20 08:17:07 -0500

    feat: test sums functions

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -28,30 +28,28 @@ func TestSumAll(t *testing.T) {
 
 func TestSumAllTails(t *testing.T) {
 
-	t.Run("make the sum of some slices", func(t *testing.T) {
-		got := SumAllTails([]int{1, 2}, []int{0, 9})
-		want := []int{2, 9}
-
+	checkSumsResult := func(t testing.TB, got, want []int) {
+		t.Helper()
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
 		}
+	}
+
+	t.Run("make the sum of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSumsResult(t, got, want)
 	})
 
 	t.Run("sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSumsResult(t, got, want)
 	})
 
 	t.Run("sum slices with one element", func(t *testing.T) {
 		got := SumAllTails([]int{3}, []int{3, 4, 5})
 		want := []int{0, 9}
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSumsResult(t, got, want)
 	})
 }
