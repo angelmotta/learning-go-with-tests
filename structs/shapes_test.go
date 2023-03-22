d54ec94c26dc0d68a27e57bfeb893c15ccb76102
Angel Motta <angel.motta@utec.edu.pe>
2023-03-22 09:25:59 -0500

    refactor: table driven tests

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -35,3 +35,23 @@ func TestArea(t *testing.T) {
 		checkArea(t, circle, want)
 	})
 }
+
+func TestAreaTableDrivenTests(t *testing.T) {
+	// Anonymous struct 'areaTests'
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
+
+}
