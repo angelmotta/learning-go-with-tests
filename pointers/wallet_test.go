aab3ab85ccf352e7ef7e8fe088f5b4cb0b036a80
Angel Motta <angel.motta@utec.edu.pe>
2023-03-25 09:06:21 -0500

    refactor: quick test helper

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -15,6 +15,13 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	assertError := func(t testing.TB, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -31,10 +38,9 @@ func TestWallet(t *testing.T) {
 		startingBalance := Bitcoin(20)
 		wallet := Wallet{startingBalance}
 		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err)
 		assertBalance(t, wallet, startingBalance)
 
-		if err == nil {
-			t.Error("wanted an error but didn't get one")
-		}
 	})
 }
