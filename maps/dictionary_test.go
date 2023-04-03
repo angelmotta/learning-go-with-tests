0d5778889a53825707b973dede483e4cfe2b9b41
Angel Motta <angel.motta@utec.edu.pe>
2023-04-03 08:22:05 -0500

    feat: maps

---
+++ maps/dictionary_test.go
@@ -0,0 +1,44 @@
+package maps
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		// In this test we are interested verifying the 'value'
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		// In this test we are interested verifying the 'error'
+		_, got := dictionary.Search("unknown")
+
+		// Check if err is null before pass it to Assertion and call err.Error()
+		if got == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		// err is not Null, so we can call err.Error()
+		assertError(t, got, ErrNotFound)
+	})
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+}
