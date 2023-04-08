6fa5ebe72fd5c357ac63918ee3e1743de37c34fd
Angel Motta <angel.motta@utec.edu.pe>
2023-04-08 13:44:58 -0500

    feat: Add word-defintion to Dictionary

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -4,7 +4,10 @@ import "errors"
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+var (
+	ErrNotFound   = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -13,4 +16,23 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	// Check if word already exist
+	_, err := d.Search(word)
+
+	// Handle response error
+	switch err {
+	case ErrNotFound:
+		// Normal case: word does not exist so insert 'new' definition
+		d[word] = definition
+	case nil:
+		// nil Error means: requested word already exist in Dictionary
+		// We can not overwrite existing word-definition
+		return ErrWordExists
+	}
+
+	// word added successfully (no error)
+	return nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -15,18 +15,54 @@ func TestSearch(t *testing.T) {
 
 	t.Run("unknown word", func(t *testing.T) {
 		// In this test we are interested verifying the 'error'
-		_, got := dictionary.Search("unknown")
+		_, gotErr := dictionary.Search("unknown")
 
 		// Check if err is null before pass it to Assertion and call err.Error()
-		if got == nil {
+		if gotErr == nil {
 			t.Fatal("expected to get an error.")
 		}
 
 		// err is not Null, so we can call err.Error()
-		assertError(t, got, ErrNotFound)
+		assertError(t, gotErr, ErrNotFound)
 	})
 }
 
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+
+		err := dictionary.Add(word, definition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test definition"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, "new test definition")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	gotDefinition, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	assertStrings(t, gotDefinition, definition)
+}
+
+// assertStrings check both strings "got" and "want" are the same
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 
